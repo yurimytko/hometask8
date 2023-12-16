2ec2372d93a7ee43c1aedd26dacabed279f87071
Yurii Mytko <108684894+yurimytko@users.noreply.github.com>
2023-12-16 14:20:01 +0200

    modified commit

--- instance.cmd
+++ instance.cmd
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 aws ec2 run-instances --image-id ami-053b0d53c279acc90 --count 1 --instance-type t2.micro --key-name key-first --security-group-ids sg-09129fe1c7ff5fc1b --user-data file://lab_8.sh
--- lab_8.sh
+++ lab_8.sh
@@ -1,46 +1,33 @@
 #!/bin/bash
 
 # Оновлення пакетів та встановлення apache2
-apt update -y
-apt install apache2 -y
+apt-get update -y
+apt-get install apache2 -y
 systemctl start apache2
 systemctl enable apache2
 
-# Створення конфігураційного файлу SSL
-cat <<EOF > site.info
-[ req ]
-prompt = no
-distinguished_name = dn
+# Генерація самопідписаного SSL сертифікату за допомогою openssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=US/ST=Ohio/L=Cleveland/O=My company/OU=My company unit/CN=www.hometask8.com/emailAddress=my-email@example.com"
 
-[ dn ]
-C=US
-ST=Ohio
-L=Cleveland
-O=My company
-OU=My company unit
-emailAddress=my-email@example.com
-CN = www.hometask8.com
-EOF
-
-# Генерація SSL сертифікату за допомогою openssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -config site.info
-
-# Створення конфігураційного файлу Apache
-cat <<EOF > apache-config.conf
-<VirtualHost *:443>
-    ServerAdmin webmaster@localhost
-    DocumentRoot /var/www/html
-    SSLEngine on
-    SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
-    SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
-</VirtualHost>
-EOF
-
-# Застосування конфігурації Apache
-cp apache-config.conf /etc/apache2/sites-available/000-default.conf
+# Активація модуля SSL
 a2enmod ssl
+
+# Створення конфігураційного файлу для Apache з SSL
+cat <<EOF > /etc/apache2/sites-available/default-ssl.conf
+<IfModule mod_ssl.c>
+    <VirtualHost _default_:443>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+        SSLEngine on
+        SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
+        SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
+    </VirtualHost>
+</IfModule>
+EOF
+
+# Активація сайту з SSL та перезавантаження Apache
 a2ensite default-ssl
 systemctl restart apache2
 
-# Створення стартової веб сторінки
+# Створення стартової веб-сторінки
 echo "<html><body><h1>Hello World!</h1></body></html>" > /var/www/html/index.html
